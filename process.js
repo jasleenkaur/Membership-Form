e6f04e3cceb4dcce2a08b23619aa7c5ca5e9f3a6
jasleenkaur <jasleen.7956@gmail.com>
2015-08-07 00:35:22 +0530

    forms field added

--- process.js
+++ process.js
@@ -1,31 +1,35 @@
   function processUser()
   {
-//iamge from webcam
+//image from webcam
 var dataImage = localStorage.getItem('imgData');
 bannerImg = document.getElementById('tableBanner');
 bannerImg.src = "data:image/png;base64," + dataImage;
+
 //......
     var parameters = window.location.search.substring(1).split("&");
-   var temp = parameters[0].split("=");
-    var F = unescape(temp[1]);
-var temp = parameters[1].split("=");
-    var L = unescape(temp[1]);
-temp = parameters[2].split("=");
-    var A = unescape(temp[1]);
-temp = parameters[3].split("=");
-    var R = unescape(temp[1]);
-temp = parameters[4].split("=");
-    var str = unescape(temp[1]);
-    var AD=str.split('+').join(' ');
-temp = parameters[5].split("=");
-    var E = unescape(temp[1]);
-temp = parameters[6].split("=");
-    var M = unescape(temp[1]);
-    document.getElementById("Fname").innerHTML = F;
-    document.getElementById("Lname").innerHTML = L; 
-    document.getElementById("Age").innerHTML = A;
-    document.getElementById("Radio").innerHTML = R;
-    document.getElementById("Address").innerHTML = AD;
-    document.getElementById("Mobile").innerHTML = E;
-document.getElementById("Mobile").innerHTML = M;
+    var temp = parameters[0].split("=");
+    var str1 = unescape(temp[1]);
+    var Name = str1.split('+').join(' ');
+    var temp = parameters[1].split("=");
+    var str2 = unescape(temp[1]);
+    var Fname = str2.split('+').join(' ');
+    var temp = parameters[2].split("=");
+    var Email = unescape(temp[1]);
+    var temp = parameters[3].split("=");
+    var str3 = unescape(temp[1]);
+    var Address=str3.split('+').join(' ');
+    temp = parameters[4].split("=");
+    var Gender = unescape(temp[1]);
+    temp = parameters[5].split("=");
+    var Age = unescape(temp[1]);
+    temp = parameters[6].split("=");
+    var Joining = unescape(temp[1]);
+    
+
+    document.getElementById("name").innerHTML = Name;
+    document.getElementById("fname").innerHTML = Fname; 
+    document.getElementById("address").innerHTML = Address;
+    document.getElementById("age").innerHTML = Age;
+    document.getElementById("email").innerHTML = Email;
+    document.getElementById("joining").innerHTML = Joining;
 }
